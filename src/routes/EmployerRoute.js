0cd9a3155dd71bc5f68b6ea0e13338a2bb828b06
eric <eric.tran@alpaca.vn>
2022-05-26 13:59:20 +0700

    Thêm role

--- src/routes/EmployerRoute.js
+++ src/routes/EmployerRoute.js
@@ -2,29 +2,32 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Redirect, Route, useHistory } from "react-router-dom";
 import { UNG_TUYEN_VIEN } from "../constansts/common";
-import { NHA_TUYEN_DUNG } from "../utils/roles";
+import { NHA_TUYEN_DUNG, QUAN_TRI_VIEN } from "../utils/roles";
 
 const EmployerRoute = ({ component: Component, ...restOfProps }) => {
-  const history = useHistory()
+  const history = useHistory();
   const isAuthenticated = useSelector(
     (state) => state.userLogin.isAuthenticated
   );
   const user = useSelector((state) => state.userLogin);
-  const roles = user?.user?.taiKhoan?.loaiTaiKhoan
+  const roles = user?.user?.taiKhoan?.loaiTaiKhoan;
   console.log("isAuthenticated employer::", user);
-  console.log("isAuthenticated employer roles::", user?.user?.taiKhoan?.loaiTaiKhoan);
+  console.log(
+    "isAuthenticated employer roles::",
+    user?.user?.taiKhoan?.loaiTaiKhoan
+  );
   useEffect(() => {
-    if(isAuthenticated && roles === NHA_TUYEN_DUNG){
-      console.log("1. co quyen")
-      history.push("/employer/dashboard")
-    }  else if(!isAuthenticated) {
+    if (isAuthenticated && roles === NHA_TUYEN_DUNG) {
+      console.log("1. co quyen");
+      history.push("/employer/dashboard");
+    } else if (!isAuthenticated) {
       // history.push("/login")
-      <Redirect to="/login"/>
-    }else if(NHA_TUYEN_DUNG !== roles){
-      history.replace("/access-denied")
+      <Redirect to="/login" />;
+    } else if (![NHA_TUYEN_DUNG, QUAN_TRI_VIEN].includes(roles)) {
+      history.replace("/access-denied");
     }
   }, [isAuthenticated, roles]);
-  
+
   return (
     <Route
       {...restOfProps}
